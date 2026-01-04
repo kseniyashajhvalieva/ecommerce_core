149c5ad87fc3d0dc5525e01e965a637a90614c19
Kseniya Shajhvalieva <kseniyashajhvalieva@gmail.com>
2026-01-04 16:05:40 +0300

    Написан тест для product.py. Добавлены догстринги и аннотации типов в классы

--- src/category.py
+++ src/category.py
@@ -1,11 +1,19 @@
 class Category:
-    category_count = 0
-    product_count = 0
+    """Класс категории товаров."""
+    category_count: int = 0
+    product_count: int = 0
 
-    def __init__(self, name: str, description: str, products: list):
-        self.name = name
-        self.description = description
-        self.products = products
+    def __init__(self, name: str, description: str, products: list) -> None:
+        """
+        Инициализация категории.
+
+        :param name: Название категории
+        :param description: Описание категории
+        :param products: Список товаров в категории
+        """
+        self.name: str = name
+        self.description: str = description
+        self.products: list = products
 
         Category.category_count += 1
         Category.product_count += len(products)
