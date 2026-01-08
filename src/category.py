d4faffc4b4a25712d7fc8808f5a0a4e9af6e2f57
Kseniya Shajhvalieva <kseniyashajhvalieva@gmail.com>
2026-01-08 17:38:53 +0300

    Изменен класс Category: приватный products, метод add_product, геттер

--- src/category.py
+++ src/category.py
@@ -13,7 +13,18 @@ class Category:
         """
         self.name: str = name
         self.description: str = description
-        self.products: list = products
+        self.__products: list = products
 
         Category.category_count += 1
         Category.product_count += len(products)
+
+    def add_product(self, product) -> None:
+        self.__products.append(product)
+        Category.product_count += 1
+
+    @property
+    def products(self) -> str:
+        result = ""
+        for product in self.__products:
+            result += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return result
