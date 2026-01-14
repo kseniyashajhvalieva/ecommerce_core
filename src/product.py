dfea10c39f5f268f03c995b135e35d89326f0813
Kseniya Shajhvalieva <kseniyashajhvalieva@gmail.com>
2026-01-14 15:44:29 +0300

    Добавлена проверка типа в __add__

--- src/product.py
+++ src/product.py
@@ -43,5 +43,7 @@ class Product:
     def __str__(self) -> str:
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
 
-    def __add__(self, other: "Product") -> float:
+    def __add__(self, other: 'Product') -> float:
+        if type(self) != type(other):
+            raise TypeError("Складывать можно только товары одного класса.")
         return self.price * self.quantity + other.price * other.quantity
