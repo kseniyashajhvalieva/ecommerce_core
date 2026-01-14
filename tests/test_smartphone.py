ec717c88054c079997aef7b475ebbb99ec3b6504
Kseniya Shajhvalieva <kseniyashajhvalieva@gmail.com>
2026-01-14 16:53:45 +0300

    Запущены линтеры и форматеры для tests/

--- tests/test_smartphone.py
+++ tests/test_smartphone.py
@@ -1,10 +1,12 @@
 from src.smartphone import Smartphone
 
+
 def test_smartphone_creation() -> None:
     """Проверка создания объекта Smartphone."""
     phone = Smartphone("Test", "Desc", 100.0, 5, 95.5, "Model", 128, "Black")
     assert phone.name == "Test"
     assert phone.efficiency == 95.5
+
 
 def test_smartphone_add_same_type() -> None:
     """Проверка сложения двух смартфонов."""
