8a38668f5c2086f23d1968c2588ec53717c3b96e
Kseniya Shajhvalieva <kseniyashajhvalieva@gmail.com>
2026-01-15 21:27:00 +0300

    добавлен игнор для mypy на импорт pytest

--- tests/test_base_product.py
+++ tests/test_base_product.py
@@ -1,4 +1,4 @@
-import pytest
+import pytest # type: ignore
 from src.base_product import BaseProduct
 
 
@@ -6,4 +6,3 @@ def test_base_product_is_abstract():
     """Проверка, что BaseProduct нельзя инстанциировать."""
     with pytest.raises(TypeError):
         BaseProduct("Test", "Desc", 100.0, 5)
-        
