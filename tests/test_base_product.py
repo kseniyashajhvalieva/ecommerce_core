8f4919259805b62e108c53edc21870271cd7b6af
Kseniya Shajhvalieva <kseniyashajhvalieva@gmail.com>
2026-01-15 21:14:10 +0300

    Добавлены тесты для BaseProduct и PrintMixin

---
+++ tests/test_base_product.py
@@ -0,0 +1,9 @@
+import pytest
+from src.base_product import BaseProduct
+
+
+def test_base_product_is_abstract():
+    """Проверка, что BaseProduct нельзя инстанциировать."""
+    with pytest.raises(TypeError):
+        BaseProduct("Test", "Desc", 100.0, 5)
+        
